167cce61226df7b324c80c823a1aef03fc40f3dc
Morgan Evans <moevans@adobe.com>
2022-09-08 11:01:48 -0600

    make more responsive; add in Reveal animation

--- js/style.js
+++ js/style.js
@@ -46,3 +46,21 @@
     new WOW().init();
 
 })(jQuery); // End of use strict
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);

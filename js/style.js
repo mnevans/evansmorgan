ecf59b2640dc334abec895549489fabf00f3c0af
Morgan Evans <moevans@adobe.com>
2022-09-08 15:49:15 -0600

    remove 538

--- js/style.js
+++ js/style.js
@@ -53,7 +53,7 @@ function reveal() {
   for (var i = 0; i < reveals.length; i++) {
     var windowHeight = window.innerHeight;
     var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
+    var elementVisible = 75;
 
     if (elementTop < windowHeight - elementVisible) {
       reveals[i].classList.add("active");
